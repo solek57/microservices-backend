2c55406d2c739006f0baa8475273a6bffab4cfb4
Suslov-DS <https://github.com/solek57/student-project>
2021-01-21 09:36:38 +0400

    backend

---
+++ src/main/java/edu/java/microservices/MicroservicesBackendApplication.java
@@ -0,0 +1,11 @@
+package edu.java.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesBackendApplication.class, args);
+    }
+}
